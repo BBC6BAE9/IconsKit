551c68f94dfce63e92a5ca3080df750ee1d5b0d8
bbc6bae9 <bbc6bae9@gmail.com>
2026-01-12 11:46:25 +0800

    fix(svg): lossless svg

--- Sources/IconsKit/Icon+SwiftUI.swift
+++ Sources/IconsKit/Icon+SwiftUI.swift
@@ -8,12 +8,17 @@ import SwiftUI
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
 public extension Image {
     
+    /// 使用此初始化器可保持 SVG 矢量特性，放大不失真
     init(fluent: FluentIcon) {
-        #if os(macOS)
-        self.init(nsImage: NSImage.fluentIcon(fluent))
-        #else
-        self.init(uiImage: UIImage(fluent: fluent))
-        #endif
+        self.init(fluent.resourceString, bundle: Bundle.module)
         self = self.renderingMode(.template)
     }
+}
+
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, *)
+#Preview {
+    Image(fluent: .bold2User)
+        .resizable()
+        .frame(width: 400, height: 400)
 }
